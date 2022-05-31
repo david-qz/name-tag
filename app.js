f11b0cfb2666786e31fd4a4df442a6b56dd17b9f
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-05-31 16:21:15 -0700

    Handle an empty input box more gracefully
    If the input string is empty the name span will collapse and make
    the name tag look bad. To fix, define the initialName and reset
    the name display to that when the input is empty.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// constants
+const initialName = 'Bob'; // If changing this make sure to also change the HTML
+
 // grab DOM elements
 const nameInput = document.getElementById('name-input');
 const nameDisplay = document.getElementById('name-display');
@@ -7,6 +10,9 @@ const nameDisplay = document.getElementById('name-display');
     // use user input to update state
     // update DOM to reflect the new state
 nameInput.addEventListener('input', () =>{
-    const name = nameInput.value;
+    let name = nameInput.value;
+    if (name === '') {
+        name = initialName;
+    }
     nameDisplay.textContent = name;
 });
