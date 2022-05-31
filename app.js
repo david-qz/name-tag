f055eefb7e0a51c1123759127e05b4e9995c0fed
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-05-31 15:12:37 -0700

    JS: Attach event listener to name-input and validate attachment

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // grab DOM elements
 const nameInput = document.getElementById('name-input');
-console.log(nameInput);
 
 // set event listeners
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
+nameInput.addEventListener('input', () =>{
+    console.log('name-input changed');
+});
