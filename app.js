f0df0185b5ddb8ace699941cbc51a2cc4d61169f
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-05-31 20:46:08 -0700

    Allow users to enter their pronouns

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const initialName = 'Bob';
 
 // grab DOM elements
 const nameInput = document.getElementById('name-input');
+const pronounInput = document.getElementById('pronoun-input');
 const nameDisplay = document.getElementById('name-display');
 const colorInput = document.getElementById('color-input');
 const nameTagDisplay = document.getElementById('name-tag');
@@ -10,17 +11,21 @@ const nameTagDisplay = document.getElementById('name-tag');
 // set the display's initial content
 nameDisplay.textContent = initialName;
 
+function onInputChanged() {
+    let name = nameInput.value || initialName;
+    let pronoun = pronounInput.value;
+    if (pronoun) { pronoun = `(${pronoun})`; }
+
+
+    nameDisplay.textContent = `${name} ${pronoun}`;
+}
+
 // set event listeners
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
-nameInput.addEventListener('input', () =>{
-    let name = nameInput.value;
-    if (name === '') {
-        name = initialName;
-    }
-    nameDisplay.textContent = name;
-});
+nameInput.addEventListener('input', onInputChanged);
+pronounInput.addEventListener('input', onInputChanged);
 
 colorInput.addEventListener('input', () => {
     const color = colorInput.value;
